cb8fd2c8c1ca21889f8e6fdf13a586bf0c316e5c
HarshGangwani <gangwaniharsh.20.it@iite.indusuni.ac.in>
2024-02-10 18:04:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import Login from './components/Login'
+// import Signup from './components/Signup'
+// import Home from './components/Home'
+import TopDestination from './components/TopDestination'
+import './css/home.css'
+
+
+function App() {
+  return (
+
+    <body>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      {/*<Home/>*/}
+      <TopDestination/>
+    </body>
+    
+  )
+}
+
+export default App
